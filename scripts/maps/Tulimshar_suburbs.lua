44c528c6a37bce14d1385f7fe4b2f73ffccee256
Hongwen Qiu <qiuhongwen@gmail.com>
2012-11-12 14:12:08 +0800

    Finish conversation of Liana.

--- scripts/maps/Tulimshar_suburbs.lua
+++ scripts/maps/Tulimshar_suburbs.lua
@@ -3,5 +3,5 @@ require "scripts/npcs/Tulimshar_suburbs/liana"
 
 atinit(function()
     npc_create("Liana", npc.Liana, GENDER_FEMALE, 82.5 * TILESIZE,
-               96.5 * TILESIZE, nil, liana_update)
+               96.5 * TILESIZE, liana_talk, liana_update)
 end)
